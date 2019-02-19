af3eeb8d071a2a72a9b9a4d8f51e17849c8e5e4e
Carlos Viales <Cviales@LAPTOP-A0R21P14>
2019-02-19 11:04:51 -0600

    Obtener MacAddress

--- BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
+++ BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,6 +14,25 @@ namespace BarcodeScanner
         public MainPage()
         {
             InitializeComponent();
+
+            //https://www.c-sharpcorner.com/code/1417/device-details-xamarin-android
+            //Android.Telephony.TelephonyManager mTelephonyMgr;
+            entMacAddress.Text = GetMACAddress();
+        }
+
+        public string GetMACAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String sMacAddress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (sMacAddress == String.Empty)// only return MAC Address from first card  
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    sMacAddress = adapter.GetPhysicalAddress().ToString();
+                }
+            }
+            return sMacAddress;
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
